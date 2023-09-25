ae3b664d8d7cdec0cb18c5e76a6bdc276eff8573
Itsdjkc <118223049+Doctorine06@users.noreply.github.com>
2023-09-25 23:26:20 +0000

    mostHigh

---
+++ src/js/component/characternode.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const PeopleCard = ({character, index}) => {
+    const { store, actions } = useContext(Context);
+    const params = useParams();
+    let char = store.people.find((item, index) => index == params.theIndex)
+
+
+  return (
+    <div className="card">
+      <img
+        className="card-img-top"
+        src={"https://github.com/tbone849/star-wars-guide/blob/master/build/assets/img/characters/" + {index} + ".jpg?raw=true"}
+        alt=""
+      ></img>
+      <div className="card-body">
+        <h5 classname="card-title">{char.name}</h5>
+        <p className="card-text">{char.gender}</p>
+        <p className="card-text">{char.height}</p>
+      </div>
+    </div>
+  );
+};
+
+PeopleCard.propTypes = {
+    match: PropTypes.object,
+  };
--- src/js/component/peoplecard.js
+++ src/js/component/peoplecard.js
@@ -1,1 +1,12 @@
+// Stripe.apiKey = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc';
 
+// PaymentLinkCreateParams params =
+//   PaymentLinkCreateParams.builder()
+//     .addLineItem(
+//       PaymentLinkCreateParams.LineItem.builder()
+//         .setPrice('price_1HKiSf2eZvKYlo2CxjF9qwbr')
+//         .setQuantity(1L)
+//         .build())
+//     .build();
+
+// PaymentLink paymentLink = PaymentLink.create(params);
