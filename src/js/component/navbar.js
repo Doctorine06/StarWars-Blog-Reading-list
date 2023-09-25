812f6961e755599e662d8bed2836f10227980bf9
Itsdjkc <118223049+Doctorine06@users.noreply.github.com>
2023-09-25 19:17:21 -0400

    Update navbar.js

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,13 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-export const Navbar = () => {
+import React, {useContext} from "react";
+import {Single} from "../views/single"
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext"
+	export const Navbar = (index) => {
+		const{store, actions } = 
+			useContext(Context);
+		let favs = store.favorites.find;
+		
 	return (
 		<nav className="navbar navbar-black bg-black mb-3">
 		<Link to="/">
 		<span className="navbar-brand mb-0 h1"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png" height={70}/></span>
 		</Link>
-		<MetaMask />
+		
 		<div className="ml-auto">
 		<div className="dropdown">
 		<button
