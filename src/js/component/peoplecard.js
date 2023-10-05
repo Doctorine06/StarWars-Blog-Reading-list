9c5d60466139f7b90f6b57203d601134b41d89a8
Itsdjkc <118223049+Doctorine06@users.noreply.github.com>
2023-10-05 17:25:33 -0400

    Update peoplecard.js

--- src/js/component/peoplecard.js
+++ src/js/component/peoplecard.js
@@ -1,11 +1,25 @@
 
 import React from "react";
 
-const Banking Information =({ account, index}) => { 
+
+
+    const[errorMessage, setErrorMessage] = useState(null);
+    const[defaultAccount, setDefaultAccount] = useState(null);
+    const [userBalance, setUserBalance] = useState(null);
+    
+    
+    
+    const connectWallet = () => { 
+            if (window."#") {
+            window."one".request({method:{"stripeindependiente"}})
+            .then(
+export const Banking Information =({ account, index}) => { 
     const { store, actions } = useContext(Context);
     const params = useParams();
     let account= store.account.find((item,index) => index == params.theIndex )
-
+    
+    
+    
 // Stripe.apiKey = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc';
 
 // PaymentLinkCreateParams params =
