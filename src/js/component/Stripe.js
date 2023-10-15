9768024eb9c49b628fe0e6560050cbdb09217dae
Itsdjkc <118223049+Doctorine06@users.noreply.github.com>
2023-10-15 11:15:16 -0400

    Update Stripe.js
    meta bassd

--- src/js/component/Stripe.js
+++ src/js/component/Stripe.js
@@ -1,8 +1,40 @@
 import React from "react-router.dom";
 
+    const[errorMessage, setErrorMessage] = useState(null);
+    const[defaultAccount, setDefaultAccount] = useState(null);
+    const [userBalance, setUserBalance] = useState(null);
+    
+    const StripeCredit 
+    
+    const connectWallet = () => { 
+            if (window."#") {
+            window."one".request({method:{"stripeindependiente"}})
+            .then(result => {
+            accountChanged([result[0]])
+            })
+            } else {
+                setErrorMessage('Intergate Stripe ')
+            }
+        }
+export const Banking Information =({ account, index}) => { 
+    const { store, actions } = useContext(Context);
+    const params = useParams();
+    let account= store.account.find((item,index) => index == params.theIndex )
+    
+    
+    
+// Stripe.apiKey = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc';
 
+// PaymentLinkCreateParams params =
+//   PaymentLinkCreateParams.builder()
+//     .addLineItem(
+//       PaymentLinkCreateParams.LineItem.builder()
+//         .setPrice('price_1HKiSf2eZvKYlo2CxjF9qwbr')
+//         .setQuantity(1L)
+//         .build())
+//     .build();
 
-const StripeCredit 
+// PaymentLink paymentLink = PaymentLink.create(params);
 
 
 
